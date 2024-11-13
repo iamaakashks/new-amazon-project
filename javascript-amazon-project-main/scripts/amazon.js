448218454ac16005ab28cbcfb865f2a205503e7c
Aakash Kumar Suman <121715040+iamaakashks@users.noreply.github.com>
2024-11-13 19:13:13 +0530

    cart.js updated

--- javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -41,17 +41,17 @@ products.map((elem, index)=>{
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary js-add-to-cart" data-product-name="${elem.name}">Add to Cart</button>
+          <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id="${elem.id}">Add to Cart</button>
         </div>
     `
 })
 
 document.querySelectorAll('.js-add-to-cart').forEach((button)=>{
   button.addEventListener('click', ()=>{
-    const productName = button.dataset.productName;
+    const productId = button.dataset.productId;
     let matchingItem;
     cart.forEach((item)=>{
-      if(productName === item.productName){
+      if(productId === item.productId){
         matchingItem = item;
       }
     });
@@ -60,7 +60,7 @@ document.querySelectorAll('.js-add-to-cart').forEach((button)=>{
       matchingItem.quantity+=1;
     }else{
       cart.push({
-        productName: productName, 
+        productId: productId, 
         quantity: 1
       })
     }
