6a44cb97e000fbfe6e825db83f71a7722e393d38
Aakash Kumar Suman <121715040+iamaakashks@users.noreply.github.com>
2024-11-16 11:37:10 +0530

    loading cart from localStorage

--- javascript-amazon-project-main/data/cart.js
+++ javascript-amazon-project-main/data/cart.js
@@ -1,13 +1,21 @@
-export let cart = [
-  {
-    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-    quantity: 1
-  },
-  {
-    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-    quantity: 3
-  }
-];
+export let cart = JSON.parse(localStorage.getItem("cart"));
+
+if (!cart) {
+  cart = [
+    {
+      productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+      quantity: 1,
+    },
+    {
+      productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+      quantity: 3,
+    },
+  ];
+}
+
+function saveToLocalStorage() {
+  localStorage.setItem("cart", JSON.stringify(cart));
+}
 
 export const addToCart = (productId) => {
   let matchingItem;
@@ -25,14 +33,16 @@ export const addToCart = (productId) => {
       quantity: 1,
     });
   }
+  saveToLocalStorage();
 };
 
-export const removeFromCart = (productId)=>{
-    let newCart = [];
-    cart.forEach((cartItem)=>{
-      if(cartItem.productId !== productId){
-        newCart.push(cartItem);
-      }
-    });
-    cart = newCart;
-}
+export const removeFromCart = (productId) => {
+  let newCart = [];
+  cart.forEach((cartItem) => {
+    if (cartItem.productId !== productId) {
+      newCart.push(cartItem);
+    }
+  });
+  cart = newCart;
+  saveToLocalStorage();
+};
