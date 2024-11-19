2ef8f37a09989e0bbf13913406db8944d585dfff
Aakash Kumar Suman <121715040+iamaakashks@users.noreply.github.com>
2024-11-19 13:14:13 +0530

    made corrections in payment summary

--- javascript-amazon-project-main/scripts/checkout/paymentSummary.js
+++ javascript-amazon-project-main/scripts/checkout/paymentSummary.js
@@ -13,33 +13,34 @@ export function renderPaymentSummary(){
     const totalBeforeTax = productPriceCents + shippingPriceCents;
     const taxCents = totalBeforeTax * 0.1;
     const totalCents = totalBeforeTax + taxCents;
-
+    let totalNumberOfItem = 0;
+    cart.forEach(item => totalNumberOfItem += item.quantity);
     document.querySelector('.js-payment-summary').innerHTML = `
     <div class="payment-summary-title">Order Summary</div>
 
           <div class="payment-summary-row">
-            <div>Items (3):</div>
-            <div class="payment-summary-money">$${(productPriceCents/100).toFixed(2)}</div>
+            <div>Items (${totalNumberOfItem}):</div>
+            <div class="payment-summary-moneMath.round(y">$${(Math.round(productPriceCents)/100).toFixed(2)}</div>
           </div>
 
           <div class="payment-summary-row">
             <div>Shipping &amp; handling:</div>
-            <div class="payment-summary-money">$${(shippingPriceCents/100).toFixed(2)}</div>
+            <div class="payment-summary-money">$${(Math.round(shippingPriceCents)/100).toFixed(2)}</div>
           </div>
 
           <div class="payment-summary-row subtotal-row">
             <div>Total before tax:</div>
-            <div class="payment-summary-money">$${(totalBeforeTax/100).toFixed(2)}</div>
+            <div class="payment-summary-money">$${(Math.round(totalBeforeTax)/100).toFixed(2)}</div>
           </div>
 
           <div class="payment-summary-row">
             <div>Estimated tax (10%):</div>
-            <div class="payment-summary-money">$${(taxCents/100).toFixed(2)}</div>
+            <div class="payment-summary-money">$${(Math.round(taxCents)/100).toFixed(2)}</div>
           </div>
 
           <div class="payment-summary-row total-row">
             <div>Order total:</div>
-            <div class="payment-summary-money">$${(totalCents/100).toFixed(2)}</div>
+            <div class="payment-summary-money">$${(Math.round(totalCents)/100).toFixed(2)}</div>
           </div>
 
           <button class="place-order-button button-primary">
